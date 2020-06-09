b69a74a1a9201659a1e5876cf406008d7a813b35
Alex <34930732+AlexPerseus@users.noreply.github.com>
2020-06-09 08:26:04 -0400

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace Server_Swapper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.popUp = new System.Windows.Forms.Label();
+            this.removePopup = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "default",
+            "eastus",
+            "centralus",
+            "southcentralus",
+            "westus",
+            "brazilsouth",
+            "northeurope",
+            "westeurope",
+            "southafricanorth",
+            "eastasia",
+            "southeastasia",
+            "australiaeast",
+            "australiasoutheast",
+            "japanwest"});
+            this.comboBox1.Location = new System.Drawing.Point(12, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(252, 21);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(12, 39);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(252, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Swap";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // popUp
+            // 
+            this.popUp.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.popUp.Location = new System.Drawing.Point(12, 89);
+            this.popUp.Name = "popUp";
+            this.popUp.Size = new System.Drawing.Size(252, 49);
+            this.popUp.TabIndex = 0;
+            this.popUp.Text = "Server Swapper Requires Rainbow Six Siege \r\nto be closed to switch servers.";
+            this.popUp.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // removePopup
+            // 
+            this.removePopup.Enabled = true;
+            this.removePopup.Interval = 1;
+            this.removePopup.Tick += new System.EventHandler(this.removePopup_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(276, 73);
+            this.Controls.Add(this.popUp);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Opacity = 0.95D;
+            this.Text = "Server Swapper";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label popUp;
+        private System.Windows.Forms.Timer removePopup;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Server_Swapper
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            popUp.Location = new Point(12, 12);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int count = 0;
+            foreach (string dir in Directory.GetDirectories(@"C:\Users\" + Environment.UserName + @"\Documents\My Games\Rainbow Six - Siege"))
+            {
+                DirectoryInfo info = new DirectoryInfo(dir);
+                var GameSettings = new IniFile(info.FullName + @"\GameSettings.ini");
+                GameSettings.Write("DataCenterHint", comboBox1.Items[comboBox1.SelectedIndex].ToString(), "ONLINE");
+                count++;
+            }
+            MessageBox.Show("Server successfuly changed for " + count + " Accounts.", "Created By AlexPerseus - Github");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.SelectedIndex = 0;
+        }
+        int tickCount = 0;
+        private void removePopup_Tick(object sender, EventArgs e)
+        {
+            if (tickCount > 100)
+                popUp.Location = new Point(popUp.Location.X, popUp.Location.Y + 2);
+            tickCount++;
+        }
+    }
+}
